e18e6ccfd45983ba91d6e47c28c7dce0e40d536b
Neo <78875228+NeoPythonProjects@users.noreply.github.com>
2021-12-15 22:40:52 +0000

    database created and csv uploaded

--- mysqldb.py
+++ mysqldb.py
@@ -1,6 +1,8 @@
 import sqlite3
 import pandas as pd
 
+#Create database
+#----------------
 def connect_to_db():
   db = 'files/db.db'
   conn = sqlite3.connect(db)
@@ -106,7 +108,7 @@ def create_table_patterns(conn, cur):
   return None
 
 def create_table_ieulrs(conn, cur):
-  sqlstr = """CREATE TABLE IF NOT EXISTS expenses (
+  sqlstr = """CREATE TABLE IF NOT EXISTS ieulrs (
     lob VARCHAR(255),
     uy INT,
     ieulr DOUBLE 
@@ -115,17 +117,9 @@ def create_table_ieulrs(conn, cur):
   conn.commit()
   return None
 
-#inserting records
-#-----------------
-def insert_record_claims(cur, claim_id, uy, paid, case_, reported, date_open,
-  date_current, lob, tradecode, dim2, dim3, claim_type, dev_q):
-  sqlstr = """INSERT INTO claims VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"""
-  cur.execute(sqlstr,(claim_id, uy, paid, case_, reported, date_open,
-  date_current, lob, tradecode, dim2, dim3, claim_type, dev_q))
-  
 
-
-
+#sub functions
+#-------------
 def show_table(tablename, limit):
   # you can't pass table names as parameters
   # concatenate to sqlstr first
@@ -146,10 +140,45 @@ def clean_table(conn, cur, tablename):
   conn.commit()
   return None
 
+
+#inserting records
+#-----------------
+def insert_record_claims(cur, claim_id, uy, paid, case_, reported, date_open, date_current, lob, tradecode, dim2, dim3, claim_type, dev_q):
+  sqlstr = """INSERT INTO claims VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"""
+  cur.execute(sqlstr,(claim_id, uy, paid, case_, reported, date_open, date_current, lob, tradecode, dim2, dim3, claim_type, dev_q))
+  return None
+
+def insert_record_premium(cur, uy, lob, tradecode, dim2, dim3, gwp,policies):
+  sqlstr = "INSERT INTO premium VALUES (?,?,?,?,?,?,?)"
+  cur.execute(sqlstr, (uy, lob, tradecode, dim2, dim3, gwp,policies))  
+  return None
+
+def insert_record_expenses(cur, uy, lob, expense):
+  sqlstr = "INSERT INTO expenses VALUES (?,?,?)"
+  cur.execute(sqlstr, (uy, lob, expense))
+  return None
+
+def insert_record_patterns(cur, lob, _3, _6, _9, _12, _15, _18, _21, _24, _27, _30, _33, _36, _39, _42, _45, _48, _51, _54, _57,_60, _63, _66, _69, _72, _75, _78, _81, _84, _87, _90,  _93, _96,_99,_102):
+  sqlstr = """INSERT INTO patterns VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"""
+  cur.execute(sqlstr,(lob, _3, _6, _9, _12, _15, _18, _21, _24, _27, _30, _33, _36, _39, _42, _45, _48, _51, _54, _57,_60, _63, _66, _69, _72, _75, _78, _81, _84, _87, _90,  _93, _96,_99,_102))
+  return None
+
+def insert_record_ieulrs(cur, uy, lob, ieulr):
+  sqlstr = "INSERT INTO ieulrs VALUES (?,?,?)"
+  cur.execute(sqlstr,(uy, lob, ieulr))
+  return None
+
 #uploading csv files
 #------------------
 #appreciate record by record is a slow way of doing it
 #live app will work of Snowflake so this test code will become obsolete
+def upload_all_tables():
+  upload_claims_csv()
+  upload_premium_csv()
+  upload_expenses_csv()
+  upload_patterns_csv()
+  upload_ieulrs_csv()
+
 
 def upload_claims_csv():
   conn = connect_to_db()
@@ -176,10 +205,124 @@ def upload_claims_csv():
       )
   conn.commit()
   conn.close()
+  return None
 
+def upload_premium_csv():
+  conn = connect_to_db()
+  cur = conn.cursor()
+  #clean tablename
+  clean_table(conn, cur, "premium")
+  df = pd.read_csv("files/premium_table_random.csv")
+  for i, row in df.iterrows():
+    insert_record_premium(
+      cur,
+      row['uy'],
+      row['lob'],
+      row['tradecode'],
+      row['dim2'],
+      row['dim3'],
+      row['gwp'],
+      row['policies']
+      )
+  conn.commit()
+  conn.close()
+  return None
+  
+def upload_expenses_csv():
+  conn = connect_to_db()
+  cur = conn.cursor()
+  #clean tablename
+  clean_table(conn, cur, "expenses")
+  df = pd.read_csv("files/expense_table_random.csv")
+  for i, row in df.iterrows():
+    insert_record_expenses(
+      cur,
+      row['uy'],
+      row['lob'],
+      row['expense']
+      )
+  conn.commit()
+  conn.close()
+  return None
+
+def upload_patterns_csv():
+  conn = connect_to_db()
+  cur = conn.cursor()
+  #clean tablename
+  clean_table(conn, cur, "patterns")
+  df = pd.read_csv("files/patterns_FTU.csv")
+  for i, row in df.iterrows():
+    insert_record_patterns(
+      cur,
+      row['lob'],
+      row['3'],
+      row['6'],
+      row['9'],
+      row['12'],
+      row['15'],
+      row['18'],
+      row['21'],
+      row['24'],
+      row['27'],
+      row['30'],
+      row['33'],
+      row['36'],
+      row['39'],
+      row['42'],
+      row['45'],
+      row['48'],
+      row['51'],
+      row['54'],
+      row['57'],
+      row['60'],
+      row['63'],
+      row['66'],
+      row['69'],
+      row['72'],
+      row['75'],
+      row['78'],
+      row['81'],
+      row['84'],
+      row['87'],
+      row['90'],
+      row['93'],
+      row['96'],
+      row['99'],
+      row['102']
+      )
+  conn.commit()
+  conn.close()
+  return None
+
+def upload_ieulrs_csv():
+  conn = connect_to_db()
+  cur = conn.cursor()
+  #clean tablename
+  clean_table(conn, cur, "ieulrs")
+  df = pd.read_csv("files/IEULRs.csv")
+  for i, row in df.iterrows():
+    insert_record_ieulrs(
+      cur,
+      row['uy'],
+      row['lob'],
+      row['ieulr']
+      )
+  conn.commit()
+  conn.close()
+  return None
 
 if __name__ == "__main__":
   pass
   #create_db()
-  upload_claims_csv()
-  show_table("claims",10)
+  #upload_all_tables()
+  #upload_patterns_csv()
+  #upload_ieulrs_csv()
+  show_table("claims",5)
+  print("--------")
+  show_table("premium",5)
+  print("--------")
+  show_table("expenses",5)
+  print("--------")
+  show_table("patterns",5)
+  print("--------")
+  show_table("ieulrs",5)
