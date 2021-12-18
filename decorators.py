8142b372bfb68b79d4fb58ddb51fc81827530265
Neo <78875228+NeoPythonProjects@users.noreply.github.com>
2021-12-18 13:40:00 +0000

    combined 2 decorators into a more generic one taking an 'action' argument interact_with_db(action)

--- decorators.py
+++ decorators.py
@@ -2,6 +2,10 @@ import functools
 import sqlite3
 
 def show_records(func):
+  """ decorator that connects to db, creates cursor, executes cursor, prints query result line by line to shell and closes db connections
+
+  func: returns a sql string where args are passed as ? as part of execute and kwargs are passed as f-string variables (cur.execute doesn't accept named arguments)
+  """
   # keep introspection intact
   @functools.wraps(func)
   def wrapper_interact_with_db(*args, **kwargs):
@@ -23,47 +27,66 @@ def show_records(func):
 
 
 def insert_record(func):
+  """ decorator that connects to db, creates cursor, executes cursor, commits to db and closes db connections
+
+  func: returns a sql string where args are passed as ? as part of execute and kwargs are passed as f-string variables (cur.execute doesn't accept named arguments)
+  """  
   @functools.wraps(func)
   def wrapper_insert_record(*args, **kwargs):
-    #connect to database
+    # connect to database
     db = 'files/db.db'
     conn = sqlite3.connect(db)
     cur = conn.cursor()
-    #execute function, gives sqlstr
+    # execute function, returns sqlstr
     sqlstr = func(*args, **kwargs)
+    # args is a tuple of positional arguments
+    # there is no need to unpack as execute needs a tuple as second argument
+    # i'd have preferred to use kwargs so that position of arguments
+    # in the calling procedure doesn't matter, but execute doesn't
+    # accept keyword arguments
     cur.execute(sqlstr, args)
     conn.commit()
     conn.close
-    #return function
+    # return func
     return func
   # return wrapper
   return wrapper_insert_record
 
 
-def save_to_db(func):
-  @functools.wraps(func)
-  def wrapper_save_to_db(*args, **kwargs):
-    # unpack kwargs and find tablename and cur
-    for k,v in kwargs.items():
-      if v == 'cur': cur = k
-      if v == 'tablename': tablename = k
-    db = 'files/db.db'
-    conn = sqlite3.connect(db)
-    cur = conn.cursor() # can't pass cur to func()
-    #clean table
-    sqlstr = f"DELETE FROM {tablename}"
-    value = func(*args, **kwargs)
-    conn.commit()
-    conn.close()
-    # return func()
-    return value
-  #return wrapper
-  return wrapper_save_to_db
-
-
 def interact_with_db(action):
-  #action can be (i) show_records, upload_csv
-  pass
-  
+  """ decorator takes 1 'action' argument.
+  It connects to db, creates cursor, executes cursor, commits to db if required and closes db connection.
 
+  decorator combines the show_records and insert_record decorators
+
+  argument: action can be 'read' or 'write'
+  'read' lists the query results line by line to shell
+  'write' writes to database
+  
+  func: returns a sql string where args are passed as ? as part of execute and kwargs are passed as f-string variables (cur.execute doesn't accept named arguments)
+  """ 
+  #combining the 2 decorators into one adding an argument to decide on which action to take 
+  # decorator takes action as argument
+  # needs an extra level that accepts the argument and return a decorator
+  #action can be (i) write or (ii) read
+  def decorator(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+      #connect to database
+      db = 'files/db.db'
+      conn = sqlite3.connect(db)
+      cur = conn.cursor()
+      #execute func
+      sqlstr= func(*args, **kwargs)
+      result = cur.execute(sqlstr, args)
+      if action == "write":
+        conn.commit()
+      elif action == "read":
+        for el in result:
+          print(el)    
+      conn.close()
+      #return frunction object
+      return sqlstr
+    return wrapper
+  return decorator
 
--- test.py
+++ test.py
@@ -2,10 +2,40 @@ import decorators as decs
 import pandas as pd
 
 
-@decs.show_records
-def show_table(tablename, limit=1000000000) -> str:
+@decs.show_records # works but want to try the more generic decorator
+#@decs.interact_with_db('read')
+def show_table_old(tablename, limit=1000000000) -> str:
   sqlstr = f"""SELECT *
   FROM {tablename}
+  LIMIT {limit}
+  """  
+  return sqlstr
+
+
+@decs.interact_with_db('read')
+def show_table(*_,tablename, limit=1000000000) -> str:
+# the generic decorator uses args in teh cur.execute statement
+# args is a tuple a positional arguments
+# the execute statment expexts the sql statement to be written using ?
+# args will then provide the values for the ?
+# Unfortuately, tablenames cannot be passed as variables using ?
+# therefore, use the f-string approach to feed intable names
+# but then the tablename should not appear in args
+# therefore, make table name a named argument, so it shows up in kwargs instead of args
+# for limit, i want a default value, so that makes it a named argument
+# which will not be in args, so pass that as a n f-string variable too
+# for table, i don't want a default value, so i need to point out that
+# this is a named argument. Do that by showing the ned of the postional
+# arguments as *_. that makes tablename a named argument
+
+# so, both tablename and limit are passed as named arguments:
+# tablename because you can't use the ? method with execute for table names
+# limit becasue i want a default value
+# there are therefore no positional arguments in args
+# this is fine as there are no ? in sql for which cur.execute is expecting an argument
+# all arguments are passed to the sql string via f-string, not ?
+  sqlstr = f"""SELECT * 
+  FROM  {tablename}
   LIMIT {limit}
   """  
   return sqlstr
@@ -24,13 +54,17 @@ def upload_csv(*_, cur, tablename):
   
   return None
 
-@decs.insert_record
+#@decs.insert_record - works, but wanbt to test the more generic decorator
+@decs.interact_with_db('write')
 def insert_record_patterns(lob, dev, FTU) -> str:
   sqlstr = """INSERT INTO patterns VALUES (?,?,?)"""
   #cur.execute(sqlstr,(lob, dev, FTU))
   return sqlstr
 
+def delete_records(table):
+  pass
+  #can use @decs.insert_record i think
 
 if __name__ == "__main__":
-  #show_table("claims", limit = 5)  
-  insert_record_patterns('test', 4, 666)
+  show_table(tablename = "claims", limit = 5)  
+ #insert_record_patterns('test', 8, 555)
